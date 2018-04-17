2374bc45da054cff6c7a9991d6107cf98991049b
Sysadmin <aermakov@umail.ucsb.edu>
2018-04-17 12:33:31 -0700

    new gui for testing

---
+++ src/steamtradingcardproject/MainGUI.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package steamtradingcardproject;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import steamtradingcardproject.view.cardView;
+
+/**
+ *
+ * @author sysadmin
+ */
+public class MainGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame guiFrame = new JFrame();
+        guiFrame.setUndecorated(true);
+        guiFrame.add(new cardView());
+        guiFrame.pack();
+        guiFrame.setBackground(new Color(0, 255, 0, 0));
+        guiFrame.setLocationRelativeTo(null);
+        ComponentResizer cr = new ComponentResizer();
+        cr.registerComponent(guiFrame);
+        guiFrame.setVisible(true);
+    }
+    
+}
---
+++ src/steamtradingcardproject/controller/Controller.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package steamtradingcardproject.controller;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sysadmin
+ */
+public class Controller {
+    public void handleClose() {
+        System.exit(0);
+    }
+
+    public void handleMinimizeAction(Container topLevelAncestor) {
+        JFrame frame = (JFrame) topLevelAncestor;
+        frame.setExtendedState(JFrame.ICONIFIED);
+    }
+
+    public void handleMaximizeAction(Container topLevelAncestor) {
+        JFrame frame = (JFrame) topLevelAncestor;
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+}
